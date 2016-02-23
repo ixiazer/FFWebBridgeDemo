3a75a28a6e3b3f419d603d3b580de70890dd3a05
ixiazer <shan.xu@freshfresh.com>
2016-02-23 16:32:11 +0800

    add pro

--- FFWebBridgeDemo/ViewController.m
+++ FFWebBridgeDemo/ViewController.m
@@ -7,16 +7,43 @@
 //
 
 #import "ViewController.h"
+#import "WebViewJavascriptBridge.h"
 
 @interface ViewController ()
-
+@property WebViewJavascriptBridge* bridge;
 @end
 
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+
+    if (_bridge) { return; }
+    
+    UIWebView* webView = [[UIWebView alloc] initWithFrame:self.view.bounds];
+    [self.view addSubview:webView];
+
+    _bridge = [WebViewJavascriptBridge bridgeForWebView:webView];
+    
+    __weak typeof(self) this = self;
+    [_bridge registerHandler:@"testObjcCallback" handler:^(id data, WVJBResponseCallback responseCallback) {
+        NSLog(@"testObjcCallback called: %@", data);
+        [this h5ActionHandle:data];
+        responseCallback(@"Response from testObjcCallback");
+    }];
+    
+    [self loadExamplePage:webView];
+}
+
+- (void)h5ActionHandle:(id)data {
+//    [_bridge callHandler:@"testJavascriptHandler" data:@{ @"foo":@"before ready" }];
+}
+
+- (void)loadExamplePage:(UIWebView*)webView {
+    NSString* htmlPath = [[NSBundle mainBundle] pathForResource:@"demo" ofType:@"html"];
+    NSString* appHtml = [NSString stringWithContentsOfFile:htmlPath encoding:NSUTF8StringEncoding error:nil];
+    NSURL *baseURL = [NSURL fileURLWithPath:htmlPath];
+    [webView loadHTMLString:appHtml baseURL:baseURL];
 }
 
 - (void)didReceiveMemoryWarning {
